b17c29e24fde510e7bc20d53c0fd763f3dff9881
Sean Huang <hyprul@ucla.edu>
2018-11-03 22:07:46 -0500

    added maps no api key

--- src/Components/JourneyMap.js
+++ src/Components/JourneyMap.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import Iframe from "react-iframe";
+import MapElement from "./MapElement";
 
 export default class JourneyMap extends Component {
 	render() {
 		return (
 			<div>
-				<h1 className="header center blue-text text-darken-4">Sky Journey</h1>
-				<hr />
+				<h1 className="header center blue-text text-darken-4">
+					Sky Journey
+				</h1>
 				<div align="center">
-					<Iframe
-						width="100%"
-						height="100vh"
-						frameborder="0"
-						style="border:0"
-						url="https://www.google.com/maps/embed/v1/place?key=
-    &q=Goldman+Sachs,NYC+NY"
-						allowfullscreen
+					<MapElement
+						googleMapURL={`https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places`}
+						loadingElement={<div style={{ height: `100%` }} />}
+						containerElement={
+							<div style={{ height: `100vh`, width: `100%` }} />
+						}
+						mapElement={<div style={{ height: `100%` }} />}
 					/>
 				</div>
 			</div>
---
+++ src/Components/MapElement.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Polyline } from "react-google-maps";
+
+const MapElement = withScriptjs(withGoogleMap((props) =>{
+   const MapStyles = [
+    {
+        "featureType": "all",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#242f3e"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0c1e34"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#263c3f"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#6b9a76"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#38414e"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#212a37"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#9ca5b3"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#746855"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#1f2835"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#f3d19c"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0c1e34"
+            }
+        ]
+    },
+    {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#d59563"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0c1e34"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#515c6d"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#17263c"
+            }
+        ]
+    }
+]
+	const pathCoordinates = [
+        { lat: 36.05298765935, lng: -112.083756616339 },
+        { lat: 36.2169884797185, lng: -112.056727493181 }
+    ];
+  return (
+      <GoogleMap
+        defaultZoom={10}
+        center={ { lat:  36.05298765935, lng: -112.083756616339 } }
+
+        defaultOptions={{ styles: MapStyles }}
+        >
+                    <Polyline
+                path={pathCoordinates}
+                geodesic={true}
+                options={{
+                    strokeColor: "#ffa300",
+                    strokeOpacity: 0.75,
+                    strokeWeight: 5,
+                    
+                }}
+            />
+      </GoogleMap>
+    );
+  }
+))
+
+export default MapElement
